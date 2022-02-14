f7ea4dc87e844affc9e31e43fd08c291074fd570
red__larva <red.x.larva@protonmail.com>
2022-02-14 19:16:39 +0545

    initial commit

---
+++ core-contracts/RewardController/src/main/java/finance/omm/score/core/reward/utils/Errors.java
@@ -0,0 +1,8 @@
+package finance.omm.score.core.reward.utils;
+
+public class Errors {
+    public static final int ERROR_NOT_CONTRACT_OWNER = 1;
+    public static final int ERROR_INVALID_TYPE_ID = 2;
+    public static final int ERROR_NOT_GENERIC = 99;
+}
+
