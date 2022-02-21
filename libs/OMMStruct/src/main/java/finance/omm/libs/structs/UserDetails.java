fcc989fbac9db8a5eb524410beb1289c948a86d5
illusioncoder7 <shubh.tiwari512@gmail.com>
2022-02-21 17:20:15 +0545

    add class for user  detail

---
+++ libs/OMMStruct/src/main/java/finance/omm/libs/structs/UserDetails.java
@@ -0,0 +1,12 @@
+package finance.omm.libs.structs;
+
+import java.math.BigInteger;
+import score.Address;
+
+public class UserDetails {
+
+    public Address _user;
+    public BigInteger _userBalance;
+    public BigInteger _totalBalance;
+    public BigInteger _decimals;
+}
