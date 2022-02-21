c15e38a224e115d9bbb19d3659a1ade3e832f074
illusioncoder7 <shubh.tiwari512@gmail.com>
2022-02-21 17:17:37 +0545

    add external methods for interface

--- libs/Score/src/main/java/finance/omm/core/score/interfaces/RewardController.java
+++ libs/Score/src/main/java/finance/omm/core/score/interfaces/RewardController.java
@@ -1,7 +1,9 @@
 package finance.omm.core.score.interfaces;
 
+import finance.omm.libs.structs.UserDetails;
 import finance.omm.libs.structs.WeightStruct;
 import java.math.BigInteger;
+import java.util.Map;
 import score.Address;
 import score.annotation.External;
 import score.annotation.Optional;
@@ -33,4 +35,49 @@ public interface RewardController {
 
     @External(readonly = true)
     public BigInteger getIntegrateIndex(String assetId, BigInteger totalSupply, BigInteger lastUpdatedTimestamp);
+
+
+    @External(readonly = true)
+    public String[] getRecipients();
+
+    @External()
+    public void handleAction(UserDetails _userAssetDetails);
+
+    @External()
+    public void disableRewardClaim();
+
+    @External()
+    public void enableRewardClaim();
+
+    @External(readonly = true)
+    public Boolean isRewardClaimEnabled();
+
+    @External
+    public void handleLPAction(Address _asset, UserDetails _userDetails);
+
+    @External(readonly = true)
+    public Map<String, ?> getDailyRewards(@Optional BigInteger _day);
+
+
+    @External(readonly = true)
+    public Map<String, ?> getRewards(Address _user);
+
+    @External
+    public void startDistribution();
+
+    @External
+    public void claimRewards(Address _user);
+
+    @External
+    public void distribute();
+
+    @External(readonly = true)
+    public BigInteger getDistributedDay();
+
+
+    @External
+    public void transferOmmToDaoFund(BigInteger _value);
+
+    @External
+    public void tokenFallback(Address _from, BigInteger _value, byte[] _data);
 }
