cbb279ed003950a8a3f8b9cd00e6467e6bdc4574
illusioncoder7 <shubh.tiwari512@gmail.com>
2022-02-21 17:19:54 +0545

    add class for totalStaked detail

--- libs/OMMStruct/src/main/java/finance/omm/libs/structs/TotalStaked.java
+++ libs/OMMStruct/src/main/java/finance/omm/libs/structs/TotalStaked.java
@@ -3,6 +3,6 @@ package finance.omm.libs.structs;
 import java.math.BigInteger;
 
 public class TotalStaked {
-    public Integer decimals;
+    public BigInteger decimals;
     public BigInteger totalStaked;
 }
