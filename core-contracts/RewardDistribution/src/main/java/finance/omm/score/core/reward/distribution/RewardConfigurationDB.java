d29f7910b9e2065c8ed84973156d11c3fc70fe34
izyak <lbackup887@gmail.com>
2022-03-16 14:22:02 +0545

    Throw on getEntity if entity not found

--- core-contracts/RewardDistribution/src/main/java/finance/omm/score/core/reward/distribution/RewardConfigurationDB.java
+++ core-contracts/RewardDistribution/src/main/java/finance/omm/score/core/reward/distribution/RewardConfigurationDB.java
@@ -204,7 +204,7 @@ public class RewardConfigurationDB {
         } else if ("lendingBorrow".equals(_rewardEntity)) {
             return RESERVE;
         }
-        return null;
+        throw RewardDistributionException.unknown("Unsupported entity "+_rewardEntity+" :: "+asset);
     }
 
 
