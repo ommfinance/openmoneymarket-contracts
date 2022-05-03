0435031f92b2a105e0060d22cca52792114cad73
cacomixtle <ismael.ruben.pr@outlook.com>
2022-05-02 21:31:20 -0500

    added DaoFund score

---
+++ core-contracts/DaoFund/src/main/java/finance/omm/score/DaoFundImpl.java
@@ -0,0 +1,60 @@
+package finance.omm.score;
+
+import finance.omm.libs.address.AddressProvider;
+import finance.omm.libs.address.Contracts;
+import finance.omm.utils.exceptions.OMMException;
+
+import java.math.BigInteger;
+
+import score.Address;
+import score.Context;
+import score.UserRevertedException;
+import score.annotation.EventLog;
+import score.annotation.External;
+import score.annotation.Optional;
+
+public class DaoFundImpl extends AddressProvider {
+
+    public static final String TAG = "Dao Fund Manager";
+    /**
+     * _addressProvider: contract address of the provider _update: allow to
+     * mimic on update event call, default to false
+     */
+    public DaoFundImpl(Address _addressProvider, @Optional boolean _update) {
+        super(_addressProvider, _update);
+        if (_update) {
+            Context.println(TAG + "| on update event:" + Context.getAddress());
+        } else {
+            Context.println(TAG + "| on install event:" + Context.getAddress());
+        }
+        Context.println(TAG + "| Started Dao Fund:" + Context.getAddress());
+    }
+
+    @EventLog(indexed = 1)
+    public void FundReceived(BigInteger _amount, Address _reserve) {
+    }
+
+    @External(readonly = true)
+    public String name() {
+        return "Omm " + TAG;
+    }
+
+    @External
+    public void transferOmm(BigInteger _value, Address _address) {
+        onlyOrElseThrow(Contracts.GOVERNANCE,
+                OMMException.of(new UserRevertedException(
+                        TAG + " | SenderNotGovernanceError: sender is not equals to governance")));
+        Address ommAddress = getAddress(Contracts.OMM_TOKEN.getKey());
+
+        if (ommAddress == null) {
+            Context.revert(TAG + "| omm address was not set");
+        }
+        Context.call(ommAddress, "transfer", _address, _value);
+    }
+
+    @External
+    public void tokenFallback(Address _from, BigInteger _value, @Optional byte[] _data) {
+        this.FundReceived(_value, Context.getCaller());
+    }
+
+}
