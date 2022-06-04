43a3d8e90e937144774f536ce6fbc41ff3741f87
olafpena <olafpena@gmail.com>
2022-06-04 13:40:24 -0500

    Fixed based on PR comments

--- libs/Score/src/main/java/finance/omm/core/score/interfaces/WorkerToken.java
+++ libs/Score/src/main/java/finance/omm/core/score/interfaces/WorkerToken.java
@@ -1,19 +1,11 @@
 package finance.omm.core.score.interfaces;
 
-import java.math.BigInteger;
 import java.util.List;
 
 import finance.omm.core.score.interfaces.token.IRC2;
 import score.Address;
-import score.annotation.Optional;
 
 public interface WorkerToken extends IRC2{
 
-    String name();
-    String symbol();
-    BigInteger decimals();
-    BigInteger totalSupply();
-    BigInteger balanceOf(Address _owner);
-    void transfer(Address _to, BigInteger _value, @Optional byte[] _data);
     List<Address> getWallets();
 }
