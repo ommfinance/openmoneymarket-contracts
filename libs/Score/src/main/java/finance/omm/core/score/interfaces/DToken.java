f1c7ca4cada935b328fb9839d79f85a39f61a50a
cacomixtle <ismael.ruben.pr@outlook.com>
2022-06-12 14:34:37 -0500

    added unit tests and IT

--- libs/Score/src/main/java/finance/omm/core/score/interfaces/DToken.java
+++ libs/Score/src/main/java/finance/omm/core/score/interfaces/DToken.java
@@ -2,52 +2,35 @@ package finance.omm.core.score.interfaces;
 
 import java.math.BigInteger;
 
+import finance.omm.core.score.interfaces.token.IRC2;
 import finance.omm.libs.structs.SupplyDetails;
 import finance.omm.libs.structs.TotalStaked;
 import score.Address;
 import score.annotation.External;
 
-public interface DToken extends AddressProvider {
+public interface DToken extends AddressProvider, IRC2{
 
     @External(readonly = true)
-    String name();
-    
-    @External(readonly = true)
-    String symbol();    
-    
-    @External(readonly = true)
-    BigInteger decimals();
-    
-    @External(readonly = true)
     BigInteger getUserBorrowCumulativeIndex(Address _user);
-    
-    @External(readonly = true)
-    BigInteger balanceOf(Address _owner);
-    
+
     @External(readonly = true)
     BigInteger principalBalanceOf(Address _user);
-    
+
     @External(readonly = true)
     BigInteger principalTotalSupply();
-    
+
     @External(readonly = true)
     SupplyDetails getPrincipalSupply(Address _user);
-    
-    @External(readonly = true)
-    BigInteger totalSupply();
-    
-    @External(readonly = true)
+
+    @External
     void mintOnBorrow(Address _user, BigInteger _amount, BigInteger _balanceIncrease);
-    
-    @External(readonly = true)
+
+    @External
     void  burnOnRepay(Address _user, BigInteger _amount, BigInteger _balanceIncrease);
-    
-    @External(readonly = true)
+
+    @External
     void burnOnLiquidation(Address _user, BigInteger _amount, BigInteger _balanceIncrease);
-    
-    @External(readonly = true)
-    void transfer(Address _to, BigInteger _value, byte[] _data);
-    
+
     @External(readonly = true)
     TotalStaked getTotalStaked();
 }
