ff8f048538d1b700a3a1024bd6c3b2ce1d1651d6
Itshyphen <ranjugc69@gmail.com>
2022-06-15 21:17:46 +0545

    folder structure for liquidation manager

---
+++ core-contracts/LiquidationManager/src/main/java/LiquidationManagerImpl.java
@@ -0,0 +1,6 @@
+import finance.omm.core.score.interfaces.LiquidationManager;
+
+public class LiquidationManagerImpl implements LiquidationManager{
+
+
+}
---
+++ libs/Score/src/main/java/finance/omm/core/score/interfaces/LiquidationManager.java
@@ -0,0 +1,5 @@
+package finance.omm.core.score.interfaces;
+
+public interface LiquidationManager {
+
+}
