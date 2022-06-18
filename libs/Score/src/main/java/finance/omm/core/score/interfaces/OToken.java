b1fa08fc348dea824c043f5208272726c0f1fa1e
cacomixtle <ismael.ruben.pr@outlook.com>
2022-06-17 20:29:57 -0500

    made some cleanup

--- libs/Score/src/main/java/finance/omm/core/score/interfaces/OToken.java
+++ libs/Score/src/main/java/finance/omm/core/score/interfaces/OToken.java
@@ -3,26 +3,21 @@ package finance.omm.core.score.interfaces;
 import java.math.BigInteger;
 import java.util.Map;
 
+import finance.omm.core.score.interfaces.token.IRC2;
 import finance.omm.libs.structs.SupplyDetails;
 import finance.omm.libs.structs.TotalStaked;
 import score.Address;
 
-public interface OToken {
+public interface OToken extends IRC2, AddressProvider{
 
-    String name();
-    String symbol();
-    BigInteger decimals();
     BigInteger principalTotalSupply();
-    BigInteger totalSupply();
     BigInteger getUserLiquidityCumulativeIndex(Address _user);
-    BigInteger balanceOf(Address _owner);
     BigInteger principalBalanceOf(Address _user);
     boolean isTransferAllowed(Address _user, BigInteger _amount);
     SupplyDetails getPrincipalSupply(Address _user);
     Map<String, Object> redeem(Address _user, BigInteger _amount);
     void mintOnDeposit(Address _user, BigInteger _amount);
     void burnOnLiquidation(Address _user, BigInteger _value);
-    void transfer(Address _to, BigInteger _value, byte[] _data);
     TotalStaked getTotalStaked();
 
 }
