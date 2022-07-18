93ac45a287e2351f7f4ae99c539f0abac0b5ad50
aanya <asdhamala@gmail.com>
2022-07-18 11:30:36 +0545

    initialized project structure

--- token-contracts/DToken/src/intTest/java/finance/omm/score/tokens/DTokenIT.java
+++ token-contracts/DToken/src/intTest/java/finance/omm/score/tokens/DTokenIT.java
@@ -1,34 +1,47 @@
 package finance.omm.score.tokens;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import finance.omm.core.score.interfaces.DToken;
-import finance.omm.core.score.interfaces.DTokenScoreClient;
+import finance.omm.libs.test.integration.OMM;
+import finance.omm.libs.test.integration.OMMClient;
 import finance.omm.libs.test.integration.ScoreIntegrationTest;
-import foundation.icon.score.client.DefaultScoreClient;
-import foundation.icon.score.client.ScoreClient;
-import java.math.BigInteger;
-import org.junit.jupiter.api.Test;
-import score.Address;
+import finance.omm.libs.test.integration.configs.Config;
+import finance.omm.libs.test.integration.configs.dTokenConfig;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DTokenIT implements ScoreIntegrationTest {
 
+    private static OMMClient ommClient;
 
-    Address addressProvider = Address.fromString("cxa755b2ef6eb46c1e817c636be3c21d26c81fe6cc");
+    @BeforeAll
+    static void setup() throws  Exception{
+        OMM omm = new OMM("conf/all-contracts.json");
 
-    DefaultScoreClient client = DefaultScoreClient.of(System.getProperties());
-
-    @ScoreClient
-    DToken scoreClient = new DTokenScoreClient(client);
+        omm.setupOMM();
+        Config config = new dTokenConfig(omm.getAddresses());
+        omm.runConfig(config);
+        ommClient = omm.defaultClient();
+    }
 
     @Test
     void ShouldGetDecimals() {
 
-        BigInteger decimals = scoreClient.decimals();
-        assertNotNull(decimals);
-        assertEquals(BigInteger.valueOf(18), decimals);
+//        BigInteger decimals = scoreClient.decimals();
+//        assertNotNull(decimals);
+//        assertEquals(BigInteger.valueOf(18), decimals);
 
-    }  
+    }
+
+    @Test
+    void testName() {
+        assertEquals("SICX Debt Token", ommClient.dICX.name());
+    }
 
 }
