02e4d5c979789562c05dc46cc6b7eb2e8ad4bb4a
aanya <asdhamala@gmail.com>
2022-07-20 18:00:50 +0545

    keep annotation on UserDetails

--- libs/OMMStruct/src/main/java/finance/omm/libs/structs/UserDetails.java
+++ libs/OMMStruct/src/main/java/finance/omm/libs/structs/UserDetails.java
@@ -2,11 +2,17 @@ package finance.omm.libs.structs;
 
 import java.math.BigInteger;
 import score.Address;
+import score.annotation.Keep;
 
 public class UserDetails {
 
+    @Keep
     public Address _user;
+    @Keep
     public BigInteger _userBalance;
+    @Keep
     public BigInteger _totalSupply;
+
+    @Keep
     public BigInteger _decimals;
 }
