1b9d32434fdcdfbc44beb529d35007e8c5e0c005
aanya <asdhamala@gmail.com>
2022-07-20 19:55:30 +0545

    deposit icx working

--- token-contracts/OToken/src/intTest/java/finance/omm/score/tokens/OTokenIT.java
+++ token-contracts/OToken/src/intTest/java/finance/omm/score/tokens/OTokenIT.java
@@ -1,5 +1,6 @@
 package finance.omm.score.tokens;
 
+import static finance.omm.utils.math.MathUtils.ICX;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import finance.omm.libs.test.integration.OMM;
@@ -7,6 +8,7 @@ import finance.omm.libs.test.integration.OMMClient;
 import finance.omm.libs.test.integration.ScoreIntegrationTest;
 import finance.omm.libs.test.integration.configs.Config;
 
+import finance.omm.libs.test.integration.scores.LendingPoolScoreClient;
 import finance.omm.score.tokens.config.oTokenConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
@@ -14,11 +16,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.math.BigInteger;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OTokenIT implements ScoreIntegrationTest {
 
     private static OMMClient ommClient;
+
+    private static OMMClient testClient;
 
     @BeforeAll
     static void setup() throws Exception {
@@ -28,11 +34,18 @@ public class OTokenIT implements ScoreIntegrationTest {
         Config config = new oTokenConfig();
         omm.runConfig(config);
         ommClient = omm.defaultClient();
+        testClient = omm.testClient();
 
     }
 
     @Test
     void testName() {
         assertEquals("SICX Interest Token", ommClient.oICX.name());
+    }
+
+    @Test
+    void deposit(){
+        ((LendingPoolScoreClient)ommClient.lendingPool).
+                deposit(BigInteger.valueOf(1000).multiply(ICX),BigInteger.valueOf(1000).multiply(ICX));
     }
 }
