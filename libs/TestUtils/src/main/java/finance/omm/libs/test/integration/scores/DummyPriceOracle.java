099ab855a79d1a7dc3fd74863780920b88c49f86
aanya <asdhamala@gmail.com>
2022-08-05 07:59:51 +0545

    interface update for lendingPool test

--- libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/DummyPriceOracle.java
+++ libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/DummyPriceOracle.java
@@ -1,5 +1,16 @@
 package finance.omm.libs.test.integration.scores;
 
+import score.annotation.External;
+
+import java.math.BigInteger;
+import java.util.Map;
+
 public interface DummyPriceOracle {
+
+    @External
+    void set_reference_data(String _base, BigInteger _price);
+
+    @External(readonly = true)
+    Map<String, BigInteger> get_reference_data(String _base, String _quote);
 
 }
--- libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/LendingPool.java
+++ libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/LendingPool.java
@@ -2,6 +2,7 @@ package finance.omm.libs.test.integration.scores;
 
 import score.Address;
 import score.annotation.External;
+import score.annotation.Optional;
 import score.annotation.Payable;
 
 import java.math.BigInteger;
@@ -17,4 +18,14 @@ public interface LendingPool {
     void setFeeSharingTxnLimit(BigInteger _limit);
 
     void borrow(Address _reserve, BigInteger _amount);
+
+    void redeem(Address _oToken, BigInteger _amount, @Optional boolean _waitForUnstaking);
+
+    void claimRewards();
+
+    void stake(BigInteger _value);
+
+    void unstake(BigInteger _value);
+
+    void tokenFallback(Address _from, BigInteger _value, byte[] _data);
 }
