2765b64e63bd710d3c28a8b8caa29c6f84d5013d
suyogadhikari <adhikarisuyog49@gmail.com>
2022-08-08 13:03:58 +0545

    lending pool data provider unit test added.

---
+++ core-contracts/LendingPoolDataProvider/src/test/java/finance/omm/score/core/finance/omm/score/core/lendingpoolDataProvider/test/unit/LendingPoolDataProviderUnitTest.java
@@ -0,0 +1,39 @@
+package finance.omm.score.core.finance.omm.score.core.lendingpoolDataProvider.test.unit;
+
+import com.iconloop.score.test.Account;
+import com.iconloop.score.test.Score;
+import com.iconloop.score.test.ServiceManager;
+import com.iconloop.score.test.TestBase;
+import finance.omm.core.score.interfaces.LendingPoolDataProvider;
+import finance.omm.libs.address.Contracts;
+import finance.omm.libs.structs.AddressDetails;
+import finance.omm.score.core.lendingpoolDataProvider.LendingPoolDataProviderImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static finance.omm.score.core.lendingpoolDataProvider.AbstractLendingPoolDataProvider.TAG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LendingPoolDataProviderUnitTest extends TestBase {
+
+
+    private static ServiceManager sm = getServiceManager();
+    private static Account owner = sm.createAccount();
+
+    private Score lendingPoolDataProvider;
+    private Account accountAddressProvider = sm.createAccount();
+
+    @BeforeEach
+    public void setup() throws Exception {
+        lendingPoolDataProvider = sm.deploy(owner, LendingPoolDataProviderImpl.class, accountAddressProvider.getAddress());
+    }
+
+    @Test
+    void testName() throws Exception {
+        assertEquals("OMM " + TAG, lendingPoolDataProvider.call("name") );
+    }
+
+}
