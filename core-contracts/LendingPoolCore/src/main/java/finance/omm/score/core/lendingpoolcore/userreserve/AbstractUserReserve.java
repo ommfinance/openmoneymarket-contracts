7105d061b884e1c7e934cd433bcb8f0cb98cd67a
suyogadhikari <adhikarisuyog49@gmail.com>
2022-08-13 13:02:47 +0545

    userReserveData updated

--- core-contracts/LendingPoolCore/src/main/java/finance/omm/score/core/lendingpoolcore/userreserve/AbstractUserReserve.java
+++ core-contracts/LendingPoolCore/src/main/java/finance/omm/score/core/lendingpoolcore/userreserve/AbstractUserReserve.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class AbstractUserReserve {
 
-    protected Map<String, BigInteger> getDataFromUserReserve(Byte prefix, UserReserveDataDB userReserve) {
+    public static Map<String, BigInteger> getDataFromUserReserve(byte[] prefix, UserReserveDataDB userReserve) {
         return Map.of(
                 "lastUpdateTimestamp", userReserve.getItem(prefix).lastUpdateTimestamp.getOrDefault(BigInteger.ZERO),
                 "originationFee", userReserve.getItem(prefix).originationFee.getOrDefault(BigInteger.ZERO)
