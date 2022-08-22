5b50ae535adbd51834b072b15350649a66790262
Anurupa <asdhamala@gmail.com>
2022-08-22 10:01:48 +0545

    project structure setup

--- libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/LendingPoolDataProvider.java
+++ libs/TestUtils/src/main/java/finance/omm/libs/test/integration/scores/LendingPoolDataProvider.java
@@ -2,7 +2,25 @@ package finance.omm.libs.test.integration.scores;
 
 import score.Address;
 
+import java.math.BigInteger;
+import java.util.Map;
+
 public interface LendingPoolDataProvider {
 
+    String name();
     void setSymbol(Address _reserve, String _sym);
+
+    Map<String, BigInteger> getReserveAccountData();
+
+    Map<String, Object> getUserAccountData(Address _user);
+
+    Map<String, BigInteger> getUserReserveData(Address _reserve, Address _user);
+
+    Map<String, Object> getReserveData(Address _reserve);
+
+    Map<String, Map<String, Object>> getAllReserveData();
+
+    Map<String,Object> test(Address reserve);
+
+    Map<String, Map<String, Object>> getAllReserveConfigurationData();
 }
