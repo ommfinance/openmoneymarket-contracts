4b8f2afcbf6ca5811e0192f98519a2cc35b924c3
Anurupa <asdhamala@gmail.com>
2023-01-11 10:28:06 +0545

    get contract address in rewards

--- core-contracts/LPStaker/src/main/java/finance/omm/score/lp/LPStakerImpl.java
+++ core-contracts/LPStaker/src/main/java/finance/omm/score/lp/LPStakerImpl.java
@@ -64,6 +64,6 @@ public class LPStakerImpl extends AbstractLPStaker {
 
     @External
     public void claimRewards() {
-        call(Contracts.LENDING_POOL, "claimRewards", Context.getCaller());
+        call(Contracts.LENDING_POOL, "claimRewards", Context.getAddress());
     }
 }
