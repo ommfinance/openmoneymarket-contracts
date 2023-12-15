1e4bb44c2ccbbbcf66e2d700ef16778c155185f5
Anurupa Dhamala <62273704+Anurupa03@users.noreply.github.com>
2023-12-15 14:38:08 +0545

    chore: db leaf node cleanup (#196)

--- token-contracts/BoostedOMM/src/main/java/finance/omm/score/tokens/BoostedOMM.java
+++ token-contracts/BoostedOMM/src/main/java/finance/omm/score/tokens/BoostedOMM.java
@@ -171,15 +171,13 @@ public class BoostedOMM extends AbstractBoostedOMM {
         LockedBalance locked = getLockedBalance(sender);
         Context.require(blockTimestamp.compareTo(locked.getEnd()) >= 0, "Withdraw: The lock didn't expire");
         BigInteger value = locked.amount;
+      
         Context.require(value.signum() > 0, "Withdraw: no locked amount");
 
-        LockedBalance oldLocked = locked.newLockedBalance();
-        locked.end = UnsignedBigInteger.ZERO;
-        locked.amount = BigInteger.ZERO;
-        this.locked.set(sender, locked);
+        this.locked.set(sender, null);
         BigInteger supplyBefore = this.supply.get();
         this.supply.set(supplyBefore.subtract(value));
-        this.checkpoint(sender, oldLocked, locked);
+        this.checkpoint(sender, locked, new LockedBalance());
         callToken("transfer", sender, value, "withdraw".getBytes());
         users.remove(sender);
         Withdraw(sender, value, blockTimestamp);
