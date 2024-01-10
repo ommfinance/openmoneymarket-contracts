4c453f35992d5be0d2ffb914988a0cf0d83333ca
Anurupa <asdhamala@gmail.com>
2024-01-10 19:55:05 +0545

    changing orders of delegation test

--- core-contracts/Delegation/src/intTest/java/finance/omm/score/core/test/DelegationIntegrationTest.java
+++ core-contracts/Delegation/src/intTest/java/finance/omm/score/core/test/DelegationIntegrationTest.java
@@ -85,7 +85,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(3)
+    @Order(6)
     /*
     check if user has default delegation after locking omm
      */
@@ -109,7 +109,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(4)
+    @Order(7)
     /*
     after user locks OMM
     user updates delegation to their desired prep
@@ -147,7 +147,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(5)
+    @Order(8)
     void updateDelagationHundredPrep(){
 
         // testClient has delegated to 10 preps
@@ -183,7 +183,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(5)
+    @Order(3)
     void updateDelegationShouldFail(){
 
         score.Address[] prepSet = Environment.preps.keySet().toArray(score.Address[]::new);
@@ -210,7 +210,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(6)
+    @Order(9)
     /*
     delegate votes to different preps
     clearPrevious set delegation to default
@@ -235,7 +235,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(7)
+    @Order(4)
     /*
     this method is called from Boosted OMM
      */
@@ -246,7 +246,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
 
     }
     @Test
-    @Order(7)
+    @Order(5)
     /*
     onBalanceUpdate is called on bOMM when omm is locked
      */
@@ -258,7 +258,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(8)
+    @Order(10)
     /*
     user votes other than contributors
     on calling initializeVoteToContributor the vote should be to contributors
@@ -275,7 +275,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(9)
+    @Order(11)
     void addContributor(){
         // contributors are the default preps
         score.Address[] contributorSet = Environment.contributors.keySet().toArray(score.Address[]::new);
@@ -301,7 +301,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(10)
+    @Order(12)
     void addAllContributor(){
 
         score.Address[] contributorSet = Environment.contributors.keySet().toArray(score.Address[]::new);
@@ -327,7 +327,7 @@ public class DelegationIntegrationTest implements ScoreIntegrationTest {
     }
 
     @Test
-    @Order(11)
+    @Order(13)
     /*
     addAllContributor() and remove of the address
     should throw exception when trying to remove same address
