3b57162b138d3d2e9417181dcef6fb2f2d1c7833
Anurupa <asdhamala@gmail.com>
2024-01-12 11:55:05 +0545

    getOrDefault added in bOMM

--- token-contracts/BoostedOMM/src/main/java/finance/omm/score/tokens/BoostedOMM.java
+++ token-contracts/BoostedOMM/src/main/java/finance/omm/score/tokens/BoostedOMM.java
@@ -67,7 +67,7 @@ public class BoostedOMM extends AbstractBoostedOMM {
 
     @External(readonly = true)
     public BigInteger getTotalLocked() {
-        return this.supply.get();
+        return this.supply.getOrDefault(BigInteger.ZERO);
     }
 
     @External(readonly = true)
